c29bd1ccd0a9f90eabf574c11b7b9e2570441a28
abz0 <143391418+abz0@users.noreply.github.com>
2026-08-24 22:02:56 +1000

    simplify press function
    - make pic_index variable at the top
    - create setting functions of gui and labels
    - minor reformat on the structure of the hint creation section

--- 11SDD - GAME GAME.py
+++ 11SDD - GAME GAME.py
@@ -23,6 +23,7 @@ current_level = 0 #current level of the game
 current_points = 0 #current number of points made during gameplay
 current_attempts = 5 #current number of available attempts to have
 incorrect_count = 0 #counts the number of incorrect guesses during gameplay
+pic_index = -1 #index used to make the picture in the gameplay
 
 #pictures that will be used in the gameplay
 word_list = [
@@ -139,6 +140,88 @@ def create_next_hint(word: str, old_hint: str):
 
     return next_hint
 
+#shows the constant components of the game round gui
+def show_constant_game_round_gui():
+    app.setLabel("message", "Guess a word from the pictures")
+
+    app.showEntry("word")
+    app.setFocus("word")
+
+    app.hideButton("Play")
+    app.hideButton("Exit")
+    app.showButton("Guess")
+    app.showButton("Skip")
+    app.disableButton("Skip")
+
+    app.showLabel("pointsattempts")
+
+#sets the pointsattempts label
+def set_pointattempts_label(points: int,
+                            num_of_attempts: int
+                            ):
+    app.setLabel("pointsattempts", "Points: " + str(points) + "        " + "Attempts: " + str(num_of_attempts))
+    
+#sets the pointsattempts label
+def set_hint_label(hint_txt: str):
+    app.setLabel("hint", "Hint: " + hint_txt)
+    
+#sets the constant components of the game round gui
+def set_inconstant_game_round_gui(level: int,
+                       picture_index: int,
+                       points: int,
+                       num_of_attempts: int
+                       ):
+    app.setLabel("heading", "Level " + str(level))
+
+    app.setImage("pic", gif_list[picture_index])
+
+    app.hideLabel("hint")
+
+    app.clearEntry("word")
+
+    set_pointattempts_label(points, num_of_attempts)
+
+#shows the end of the game
+def show_end_of_game_gui(level: int,
+                         points: int,
+                         num_of_incorrects: int,
+                         ):
+    app.setLabel("message", "You got " + str(points) + "/70 points with " + str(num_of_incorrects) + "/40 mistakes.")
+
+    app.hideLabel("hint")
+    '''
+    app.showLabel("hint")
+    if num_of_incorrects > 30:
+        app.setLabel("hint", "(Pretty bad with so much incorrects.)")
+    elif num_of_incorrects in range(11,30):
+        app.setLabel("hint", "(Good job.)")
+    elif num_of_incorrects in range(6,10):
+        app.setLabel("hint", "(Closer to perfect.)")
+    elif num_of_incorrects in range(1,5):
+        app.setLabel("hint", "(Nearly aced it!)")
+    else:
+        app.setLabel("hint", "(WOW! That's a perfect score!)")
+    '''
+
+    app.hideEntry("word")
+
+    app.hideLabel("pointsattempts")
+
+    app.hideButton("Guess")
+    app.hideButton("Skip")                
+    app.showButton("Play")
+    app.showButton("Exit")
+    app.setButton("Play", "Play again?")
+
+    if level >= 11:
+        app.setLabel("heading", "You made it to the finish line!")
+        app.setImage("pic", "goodending.gif")
+        app.infoBox("ending", "!!! YOU GOT VICTORY !!!")
+    else:
+        app.setLabel("heading", "Too bad. You ran out of attempts.")
+        app.setImage("pic", "badending.gif")
+        app.infoBox("ending", "YOU FAIL")
+
 #behaviour of the buttons
 def press(button):
     global current_level
@@ -147,10 +230,11 @@ def press(button):
     global incorrect_count
     global word_list
     global hint
-    global pic_index #pic index used for the gameplay
+    global pic_index
 
     #when player start or restart game
     if button == "Play":
+        #sets game variables
         current_level = 1
         current_points = 0
         current_attempts = 5
@@ -158,24 +242,12 @@ def press(button):
         pic_index = get_pic_index(current_level)
 
         #sets game gui
-        app.setLabel("heading", "Level " + str(current_level))
-        app.setLabel("message", "Guess a word from the pictures")
-
-        app.setImage("pic", gif_list[pic_index])
-
-        app.hideLabel("hint")
-        
-        app.showEntry("word")
-        app.setFocus("word")
-        
-        app.hideButton("Play")
-        app.hideButton("Exit")
-        app.showButton("Guess")
-        app.showButton("Skip")
-        app.disableButton("Skip")
-
-        app.showLabel("pointsattempts")
-        app.setLabel("pointsattempts", "Points: " + str(current_points) + "           " + "Attempts: " + str(current_attempts))
+        show_constant_game_round_gui()
+        set_inconstant_game_round_gui(current_level,
+                                    pic_index,
+                                    current_points,
+                                    current_attempts
+                                    )
 
     #when players guess a word
     elif button == "Guess":
@@ -202,15 +274,11 @@ def press(button):
                 pic_index = get_pic_index(current_level)
 
                 #next level of the game gui
-                app.setLabel("heading", "Level " + str(current_level))
-                
-                app.setImage("pic", gif_list[pic_index])
-
-                app.hideLabel("hint")
-
-                app.clearEntry("word")
-                
-                app.setLabel("pointsattempts", "Points: " + str(current_points) + "        " + "Attempts: " + str(current_attempts))
+                set_inconstant_game_round_gui(current_level,
+                                            pic_index,
+                                            current_points,
+                                            current_attempts
+                                            )
 
             #when the player guesses incorrectly
             else:
@@ -228,62 +296,31 @@ def press(button):
 
                 #updates the game gui after incorrect guesses
                 app.clearEntry("word")
+                set_pointattempts_label(current_points, current_attempts)
                 
-                app.setLabel("pointsattempts", "Points: " + str(current_points) + "        " + "Attempts: " + str(current_attempts))
-
             #displays the hint when the current available attempts are 3 and below
-            if current_attempts == 3:
-                hint = create_first_hint(word_list[pic_index])
-                
-                app.showLabel("hint")
-                app.setLabel("hint", "Hint: " + hint)
-            elif current_attempts == 2:
-                hint = create_next_hint(word_list[pic_index], hint)
-
-                app.setLabel("hint", "Hint: " + hint)
-            elif current_attempts == 1:
-                for i in range(2):
+            if current_attempts <= 3:
+                if current_attempts == 3:
+                    hint = create_first_hint(word_list[pic_index])
+                    
+                    app.showLabel("hint")
+                elif current_attempts == 2:
                     hint = create_next_hint(word_list[pic_index], hint)
-                
-                app.setLabel("hint", "Hint: " + hint)
+                elif current_attempts == 1:
+                    for i in range(2):
+                        hint = create_next_hint(word_list[pic_index], hint)
+                        
+                set_hint_label(hint)
 
             #when the player reaches the end of the game 
             if current_level == 11 or current_attempts == 0:
                 used_pics.clear() #clears the used_pics array
 
                 #displays the end game gui
-                app.setLabel("message", "You got " + str(current_points) + "/70 points with " + str(incorrect_count ) + "/40 mistakes.")
-
-                app.showLabel("hint")
-                if incorrect_count > 30:
-                    app.setLabel("hint", "(Pretty bad with so much incorrects.)")
-                elif incorrect_count in range(11,30):
-                    app.setLabel("hint", "(Good job.)")
-                elif incorrect_count in range(6,10):
-                    app.setLabel("hint", "(Closer to perfect.)")
-                elif incorrect_count in range(1,5):
-                    app.setLabel("hint", "(Nearly aced it!)")
-                else:
-                    app.setLabel("hint", "(WOW! That's a perfect score!)")
-
-                app.hideEntry("word")
-
-                app.hideLabel("pointsattempts")
-                
-                app.hideButton("Guess")
-                app.hideButton("Skip")                
-                app.showButton("Play")
-                app.showButton("Exit")
-                app.setButton("Play", "Play again?")
-
-                if current_level == 11:
-                    app.setLabel("heading", "You made it to the finish line!")
-                    app.setImage("pic", "goodending.gif")
-                    app.infoBox("ending", "!!! YOU GOT VICTORY !!!")
-                else:
-                    app.setLabel("heading", "Too bad. You ran out of attempts.")
-                    app.setImage("pic", "badending.gif")
-                    app.infoBox("ending", "YOU FAIL")
+                show_end_of_game_gui(current_level,
+                                     current_points,
+                                     incorrect_count
+                                     )
 
     #when player wants to exit the game
     elif button == "Exit":
@@ -311,7 +348,7 @@ app.addButtons(["Guess", "Skip"], press)
 app.hideButton("Guess")
 app.hideButton("Skip")
 
-app.addLabel("pointsattempts", "Points: " + str(current_points) + "           " + "Attempts: " + str(current_attempts))
+app.addLabel("pointsattempts", "Points and attempts unavailable")
 app.hideLabel("pointsattempts")
 
 #executes the games
